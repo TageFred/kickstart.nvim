44d124a70dacd8f8ac92cf2f51a0b4b0a12ee1d8
Birch frasmusson <rasmussonf@gmail.com>
2024-02-10 21:33:01 +0100

    New branch test

--- init.lua
+++ init.lua
@@ -602,7 +602,7 @@ local harpoon = require("harpoon")
 harpoon:setup({})
 
 vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-o>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>n", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
@@ -610,8 +610,8 @@ vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-H>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-L>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<S-H>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<S-L>", function() harpoon:list():next() end)
 
 
 -- The line beneath this is called `modeline`. See `:help modeline`
